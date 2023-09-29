93c7b028e4f36ccb757ddc6082341cee86c54228
vitroid <vitroid@gmail.com>
2023-09-29 13:40:58 +0900

    -

--- andersan/tile.py
+++ andersan/tile.py
@@ -25,16 +25,18 @@ def get_tile_num(lat, lon, zoom):
     # https://sorabatake.jp/7325/
     # https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Python
     lat_rad = np.radians(lat)
-    n = 2.0 ** zoom
+    n = 2.0**zoom
     xtile = int((lon + 180.0) / 360.0 * n)
-    ytile = int((1.0 - np.log(np.tan(lat_rad) + (1 / np.cos(lat_rad))) / np.pi) / 2.0 * n)
+    ytile = int(
+        (1.0 - np.log(np.tan(lat_rad) + (1 / np.cos(lat_rad))) / np.pi) / 2.0 * n
+    )
     return (xtile, ytile)
 
 
 def num2deg(xtile, ytile, zoom):
     # https://sorabatake.jp/7325/
     # https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Python
-    n = 2.0 ** zoom
+    n = 2.0**zoom
     lon_deg = xtile / n * 360.0 - 180.0
     lat_rad = np.arctan(np.sinh(np.pi * (1 - 2 * ytile / n)))
     lat_deg = np.degrees(lat_rad)
@@ -63,7 +65,7 @@ def get_tile_bbox(z, x, y):
 
     right_top = num2deg(x + 1, y, z)
     left_bottom = num2deg(x, y + 1, z)
-    return (left_bottom[0], left_bottom[1], right_top[0], right__top[1])
+    return (left_bottom[0], left_bottom[1], right_top[0], right_top[1])
 
 
 def get_tile_approximate_lonlats(z, x, y):
@@ -88,15 +90,15 @@ def get_tile_approximate_lonlats(z, x, y):
     bbox = get_tile_bbox(z, x, y)
     width = abs(bbox[2] - bbox[0])
     height = abs(bbox[3] - bbox[1])
-    width_per_px = width/256
-    height_per_px = height/256
+    width_per_px = width / 256
+    height_per_px = height / 256
 
-    lonlats = np.zeros((256,256,2))
+    lonlats = np.zeros((256, 256, 2))
     lat = bbox[3]
     for i in range(256):
         lon = bbox[0]
         for j in range(256):
-            lonlats[i,j,:] = [lon, lat]
+            lonlats[i, j, :] = [lon, lat]
             lon += width_per_px
         lat -= height_per_px
     return lonlats
@@ -106,8 +108,8 @@ def test():
     # 平塚市の中心部のタイルは 13/7266/3235
     lon, lat = num2deg(7266, 3235, zoom=13)
     print(lon, lat)
-    x,y = get_tile_num(lat+0.00001, lon-0.00001, zoom=13)
-    print(x,y)
+    x, y = get_tile_num(lat + 0.00001, lon - 0.00001, zoom=13)
+    print(x, y)
 
 
 # test
