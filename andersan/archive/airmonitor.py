1a59cc5c06ed9dd79c981d57c93efbaa0d18932a
vitroid <vitroid@gmail.com>
2025-02-27 22:37:03 +0900

    Archive of air monitor is included.

--- andersan/archive/airmonitor.py
+++ andersan/archive/airmonitor.py
@@ -3,26 +3,23 @@ import datetime
 
 import pandas as pd
 import numpy as np
+import json
 
 from andersan import tile
 from airpollutionwatch.convert import stations as fullstations
 from delaunayextrapolation import DelaunayE
 from airpollutionwatch import kanagawa, shizuoka, tokyo, chiba, yamanashi
 
-# import os
-# import requests_cache
-# from retry_requests import retry
-
+# archive/airmonitor.pyはgrid12の値を返すか、あるいは局ごとのデータ(items/)からその場で三角メッシュを切り、
+# 結果を
 
 try:
     from .sqlitedictcache import sqlitedict_cache
     from .__init__ import Neighbors, prefecture_ranges
-    from . import amedas
 except:
     # for test()
     from andersan.sqlitedictcache import sqlitedict_cache
-    from __init__ import Neighbors, prefecture_ranges
-    import amedas
+    from andersan import Neighbors, prefecture_ranges
 
 
 # 県ごとの大気監視ウェブサイトからデータをもってくる関数の名前
@@ -55,13 +52,12 @@ def wdws2wxwy(wdws):
 
 
 # @lru_cache(maxsize=9999)
-# @shelf_cache("observes")
-@sqlitedict_cache("observes")  # vscodeで中身をチェックできる分、こちらのほうが便利
-def observes_(
+# @shelf_cache("airmonitor")
+@sqlitedict_cache("archive_airmonitor")  # vscodeで中身をチェックできる分、こちらのほうが便利
+def tiles_(
     target_prefecture: str,
     isodate: str,
     zoom: int,
-    use_amedas=False,
     items=["NMHC", "OX", "NOX", "TEMP", "WX", "WY"],  # order in datatype3
 ):
     """各県の特定時刻の大気監視データを入手し、地理院メッシュ点での測定値を内挿する。"""
@@ -73,29 +69,23 @@ def observes_(
     pref_range = np.array(prefecture_ranges[target_prefecture])  # lon,lat
     tiles, shape = tile.tiles(zoom, pref_range)
 
+    unixtime = int(datetime.datetime.fromisoformat(isodate).timestamp())
     # 測定値をとってくる。
-    # 2回目からのアクセスはairpollution.sqliteに保存された内容を利用する
-    try:
-        dfs = [
-            prefecture_retrievers[pref].retrieve(isodate)
-            for pref in Neighbors[target_prefecture]
-        ]
-    except:
-        return None
-
-    if use_amedas:
-        # 気温はAMeDASから入手 (ゆくゆくは風速も)
-        amedas_df = amedas.retrieve(isodate)
-        amedas_df = amedas_df.replace({pd.NA: None})
-        amedas_df["WX"], amedas_df["WY"] = wdws2wxwy(
-            amedas_df[["WD", "WS"]].to_numpy().astype(float)
-        )
-
+    BASE="/AIR/edamame2/items"
+    full = pd.DataFrame()
+    for item in items:
+        with open(f"{BASE}/{item}/{unixtime}/stations.json") as j:
+            d = json.load(j)
+            # これは遅い処理。もっと簡潔にできる。
+            for station, value in d.items():
+                full.loc[int(station), item] = value
+    
     # 全県の測定値を連結。欠測はNaNとする。
-    full = pd.concat(dfs, join="outer")
     full = full.replace({pd.NA: None})
-    full["WX"], full["WY"] = wdws2wxwy(full[["WD", "WS"]].to_numpy().astype(float))
-
+    
+    
+    # 以下は../airmonitor.pyと同じはず。(amedasを使うところが省かれているけど)
+    
     # 神奈川県の範囲
     # 範囲の指定方法を変更
     lonlats = tile.lonlat(zoom=zoom, xy=tiles)
@@ -124,13 +114,6 @@ def observes_(
         # 副作用をさける
         del series
 
-        # Mix amedas
-        if use_amedas:
-            if item in ("TEMP", "WX", "WY"):
-                series2 = amedas_df[["lon", "lat", item]].dropna()
-                item_df = pd.concat([item_df, series2])
-                # print(item_df.tail())
-
         # 測定局でDelaunay三角形を作り、gridsの格子点の内挿比を求める
         tri = DelaunayE(item_df[["lon", "lat"]])
 
@@ -151,17 +134,16 @@ def observes_(
     return table
 
 
-def observes(
+def tiles(
     target_prefecture: str,
     isodate: str,
     zoom: int,
-    use_amedas=True,
     items=["NMHC", "OX", "NOX", "TEMP", "WX", "WY"],  # order in datatype3
 ):  # ここで、isodateに時刻が含まれる場合に日付と時だけに修正する。
     dt = datetime.datetime.fromisoformat(isodate)
     datestr = dt.strftime("%Y-%m-%dT%H:00:00+09:00")
-    return observes_(
-        target_prefecture, datestr, zoom, use_amedas=use_amedas, items=items
+    return tiles_(
+        target_prefecture, datestr, zoom, items=items
     )
 
 
@@ -169,11 +151,8 @@ def test():
     basicConfig(level=DEBUG)
     logger = getLogger()
     logger.info(
-        observes("kanagawa", "2025-02-20T22:00+09:00", zoom=12, use_amedas=True)
-    )  # worked on 2025-02-20
-    logger.info(
-        observes("kanagawa", "2025-02-20T22:00+09:00", zoom=12, use_amedas=False)
-    )  # worked on 2025-02-20
+        tiles("kanagawa", "2015-02-20T22:00+09:00", zoom=12)
+    )
 
 
 if __name__ == "__main__":
